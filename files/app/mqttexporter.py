27aa39ae2169e8ee0355b8d8c2638c7ce9a862b6
Sander Revenberg <sander@revenberg.info>
2021-11-12 12:42:13 +0100

    .

--- files/app/mqttexporter.py
+++ files/app/mqttexporter.py
@@ -158,7 +158,7 @@ def _parse_message(topic, payload):
 
 def expose_metrics(client, userdata, msg):  # pylint: disable=W0613
     """Expose metrics to prometheus when a message has been published (callback)."""
-    if msg.topic in IGNORED_TOPICS:
+    if msg.topic in IGNORED_TOPICS or IGNORED_TOPICS in msg.topic:
         LOG.debug('Topic "%s" was ignored', msg.topic)
         return
 
